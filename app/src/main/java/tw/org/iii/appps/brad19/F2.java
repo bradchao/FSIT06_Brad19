666be74e49f3c2280465074ee4284dc6e493edfa
Brad <brad@brad.tw>
2019-08-10 14:32:11 +0800

    5

--- app/src/main/java/tw/org/iii/appps/brad19/F2.java
+++ app/src/main/java/tw/org/iii/appps/brad19/F2.java
@@ -9,18 +9,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class F2 extends Fragment {
+    private View mainView;
+    private TextView tv;
+    private Timer timer;
+    private int iCounter;
+
+    public F2(){
+        timer = new Timer();
+        timer.schedule(new MyTask(), 0, 1000);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f2, container, false);
+
+        mainView = inflater.inflate(R.layout.fragment_f2, container, false);
+        tv = mainView.findViewById(R.id.f2_tv);
+
+        return mainView;
+    }
+
+    private class MyTask extends TimerTask {
+        @Override
+        public void run() {
+            if (tv !=null){
+                iCounter++;
+            }
+        }
     }
 
     @Override
